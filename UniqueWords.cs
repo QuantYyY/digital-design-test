76106b79e1ba4105a96547037fad4a782e1cad99
Maksim Khirug <maxkhirug@gmail.com>
2023-05-26 19:45:28 +0300

    C# task-3

---
+++ UniqueWords.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_Api
+{
+    public class UniqueWords
+    {
+        public Dictionary<string, int> SortedDictionary { get; set; }
+
+    }
+}
---
+++ WebApi/Controllers/GetUniqueWordsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GetUniqueWords;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetUniqueWordsController : ControllerBase
+    {
+        [HttpPost(Name = "GetUniqueWords")]
+        public ActionResult<Dictionary<string, int>> Post([FromBody] string text)
+        {
+            UniqueFinder finder = new UniqueFinder();
+
+            var result = finder.GetUniqueWordsMultiThread(text);
+
+            return Ok(result);
+        }
+    }
+}
